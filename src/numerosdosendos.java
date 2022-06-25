f6a3e988facd6de4ae9a78d647c3090c91e5a327
ronal2003 <107308574+ronal2003@users.noreply.github.com>
2022-06-25 08:54:30 -0500

    numeros

---
+++ src/numerosdosendos.java
@@ -0,0 +1,9 @@
+public class numerosdosendos {
+    public static void main(String[] args) {
+        for(int i=2; i<=100; i = i + 2){
+            System.out.println("numeros: " +i);
+
+        }
+    }
+}
+
