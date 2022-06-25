b73e00376f3543ccdf2dc242d1363ce362ee8655
ronal2003 <107308574+ronal2003@users.noreply.github.com>
2022-06-25 09:15:07 -0500

    numeros aleatorio 1 y 3

---
+++ src/Numerosaleatorios.java
@@ -0,0 +1,11 @@
+public class Numerosaleatorios {
+    public static void main(String[] args) {
+        for(int i=0; i<=2; i++){
+            System.out.println(1);
+            System.out.println(3);
+
+
+
+        }
+    }
+}
